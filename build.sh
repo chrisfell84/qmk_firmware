f61cab5d5fc3b3acbc115dcf78d67b7902ff6708
Chris Fell <chris.fell@rea-group.com>
2021-03-05 12:18:32 +1100

    Update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 # planck (not working)
-make planck-rev4-chrisfell
-sudo dfu-programmer atmega32u4 erase
-sudo dfu-programmer atmega32u4 flash ~/Projects/planck_firmware/.build/planck_rev4_chrisfell.hex
+#make planck-rev4-chrisfell
+#sudo dfu-programmer atmega32u4 erase
+#sudo dfu-programmer atmega32u4 flash ~/Projects/planck_firmware/.build/planck_rev4_chrisfell.hex
 
 # preonic
-make preonic/rev3:chrisfell:flash
+make preonic/rev3:chrisfell
+
+# ergodox
+make ergodox_infinity:chrisfell
+
+# to flash:
+#make preonic/rev3:chrisfell:flash
